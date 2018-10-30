0fb79500afd4fb5acb79a8ca7811110cc819a286
SamarthRaval <samraval111194@gmail.com>
2018-10-30 19:00:30 -0300

    First commit, translated from original repository

---
+++ app/src/main/java/com/example/pauld/morsecode/LoginScreen_1.java
@@ -0,0 +1,17 @@
+package com.example.pauld.morsecode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginScreen_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen_1);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
+
