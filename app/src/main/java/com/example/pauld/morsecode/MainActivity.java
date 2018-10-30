cc84dd835242299f23ce6a1ec54a3d6af7510d09
Paul Duchesne <paulduchesne1337@gmail.com>
2018-10-30 17:20:19 -0300

    Pre-merging code settings code

--- app/src/main/java/com/example/pauld/morsecode/MainActivity.java
+++ app/src/main/java/com/example/pauld/morsecode/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.pauld.morsecode;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,15 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnSettings=findViewById(R.id.btnSettings);
-        btnSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent settings=new Intent(MainActivity.this,SettingsActivity.class);
-                startActivity(settings);
-            }
-        });
-
 
     }
 
