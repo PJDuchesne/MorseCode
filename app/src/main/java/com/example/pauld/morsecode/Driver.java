0f9abc9279c1efeed7e422cf39ee4ab869f67921
georgefaraj <georgemilio@gmail.com>
2018-11-18 10:09:19 -0400

    Added camera flash feature to driver

--- app/src/main/java/com/example/pauld/morsecode/Driver.java
+++ app/src/main/java/com/example/pauld/morsecode/Driver.java
@@ -1,34 +1,12 @@
 package com.example.pauld.morsecode;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.Camera;
-import android.graphics.SurfaceTexture;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.CameraMetadata;
-import android.hardware.camera2.CaptureRequest;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
-import android.media.MediaPlayer;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.media.ToneGenerator;
-import android.net.Uri;
 import android.os.Vibrator;
-import android.support.v4.app.ActivityCompat;
-import android.util.Log;
-import android.util.Size;
-import android.view.Surface;
-
-import java.util.ArrayList;
-import java.util.List;
-
 //https://progur.com/2016/12/how-to-create-8-bit-music-on-android.html
 //https://www.soundjay.com/beep-sounds-1.html
 //https://stackoverflow.com/questions/27420594/android-5-camera2-use-only-flash
@@ -41,9 +19,9 @@ public class Driver {
     private byte soundData[];
 
 
-    public Driver(Vibrator vibratorService, CameraManager cameraService) {
+    public Driver(Vibrator vibratorService, CameraManager systemService, Context applicationContext) {
         vibrateHandler = vibratorService;
-        generateSound(540, 1000);
+        generateSound(540, 10);
         soundHandler = new AudioTrack(AudioManager.STREAM_MUSIC,
                 44100,
                 AudioFormat.CHANNEL_OUT_DEFAULT,
@@ -51,7 +29,12 @@ public class Driver {
                 AudioTrack.MODE_STATIC
         );
         soundHandler.write(soundData, 0, soundData.length);
-        lightHandler = cameraService;
+        if( applicationContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)){
+            lightHandler = systemService;
+        }
+        else{
+            lightHandler = null;
+        }
         //Uri sound = Uri.parse("android.resource://" + packageName + "/" + R.raw.beep);
         //soundHandler = MediaPlayer.create(applicationContext,sound);
         //soundHandler.setLooping(false);
@@ -59,13 +42,6 @@ public class Driver {
 
     public void on(){
         vibrateHandler.vibrate(999999999 );
-
-        try{
-            String cameraId = lightHandler.getCameraIdList()[0]; // Usually front camera is at 0 position.
-            lightHandler.setTorchMode(cameraId, true);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
         /*
         try {
             soundHandler.seekTo(0);
@@ -76,6 +52,16 @@ public class Driver {
         */
         if(soundHandler.getPlayState() != AudioTrack.PLAYSTATE_PLAYING){
             soundHandler.play();
+        }
+
+        if( lightHandler != null){
+            try{
+                //0 Corresponds to the back camera
+                String cameraId = lightHandler.getCameraIdList()[0];
+                lightHandler.setTorchMode(cameraId, true);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
         }
     }
 
@@ -93,11 +79,14 @@ public class Driver {
            soundHandler.write(soundData, 0, soundData.length);
         }
 
-        try{
-            String cameraId = lightHandler.getCameraIdList()[0]; // Usually front camera is at 0 position.
-            lightHandler.setTorchMode(cameraId, false);
-        } catch (Exception e){
-            e.printStackTrace();
+        if( lightHandler != null){
+            try{
+                //0 Corresponds to the back camera
+                String cameraId = lightHandler.getCameraIdList()[0];
+                lightHandler.setTorchMode(cameraId, false);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
         }
     }
 
