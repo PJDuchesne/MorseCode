3eb253a3937d30a9e39c78d300025d0dc41feca7
FromTom <From.Tom.Smith@gmail.com>
2018-11-21 13:20:05 -0400

    Attached brain to the freestyle UI

--- app/src/main/java/com/example/pauld/morsecode/FreestyleInput.java
+++ app/src/main/java/com/example/pauld/morsecode/FreestyleInput.java
@@ -1,8 +1,12 @@
 package com.example.pauld.morsecode;
 
+import android.content.Context;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,6 +21,8 @@ public class FreestyleInput extends AppCompatActivity {
     private double screenHeight, screenWidth;
     private final double screenReferenceHeight = 2076;
     private final double screenReferenceWidth = 1080;
+
+    private MorseBrain brain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +43,49 @@ public class FreestyleInput extends AppCompatActivity {
         inputButton = findViewById(R.id.freestyle_screen_button);
         addLetterButton = findViewById(R.id.freestyle_add_letter_btn);
         resetCurrentInput = findViewById(R.id.freestyle_reset_input_btn);
+        progressbar = findViewById(R.id.freestyle_progress_bar);
 
         formatSizes();
+
+        Driver feedbackDriver = new Driver( (Vibrator) this.getSystemService(VIBRATOR_SERVICE),(CameraManager) getSystemService(Context.CAMERA_SERVICE),getApplicationContext());
+        brain = new MorseBrain(this, predictedMorse, predictedInput, totalInput, progressbar, feedbackDriver);
+        brain.ElectricShock();
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                brain.ResetCurrentChar();
+            }
+        });
+
+        inputButton.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent){
+                switch(motionEvent.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        brain.StartInput();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        brain.EndInput();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        addLetterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                brain.SignalCharEnd();
+            }
+        });
+
+        resetCurrentInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                brain.ElectricShock();
+            }
+        });
     }
 
     private void formatSizes(){
