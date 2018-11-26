a1194df08414a6e531ac42d6bea0f93f69962fca
Paul Duchesne <paulduchesne1337@gmail.com>
2018-11-25 23:02:18 -0400

    Paul's commented code

--- app/src/main/java/com/example/pauld/morsecode/MorseTrieBranch.java
+++ app/src/main/java/com/example/pauld/morsecode/MorseTrieBranch.java
@@ -1,5 +1,13 @@
 package com.example.pauld.morsecode;
 
+/**
+ * MorseTrieBranch
+ *     This class is used by the MorseTrie as the branches of the trie structure that is iterated
+ *     through when inputting Morse code into the MorseBrain class. The default constructor is
+ *     recursive and instantiates the entire tree below a given starting point.
+ *
+ *     @author Paul Duchesne B00332119
+ **/
 public class MorseTrieBranch {
     public String BranchChar;
     public String BranchMorseCode;
